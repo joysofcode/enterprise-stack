8293d9bfad4a3168198b04f63f93129968c73cc0
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-04-14 12:20:48 -0400

    provides correct types for the user in the page store

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,21 +2,23 @@
 // for information about these interfaces
 
 /// <reference types="lucia-auth" />
-declare namespace Lucia {
-	type Auth = import('$lib/server/auth').Auth
-	type UserAttributes = {
-		username: string
-	}
-}
-
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
 			auth: import('lucia-auth').AuthRequest
 		}
-		// interface PageData {}
+		interface PageData {
+			user: import('lucia-auth').User
+		}
 		// interface Platform {}
+	}
+
+	namespace Lucia {
+		type Auth = import('$lib/server/auth').Auth
+		type UserAttributes = {
+			username: string
+		}
 	}
 }
 
