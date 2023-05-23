8fe23c48c3961645504d48b96d1ee8f1579dfac1
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-05-23 12:06:38 +0900

    format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="mt-16 grid justify-items-center text-center">
 	<h1>
 		<span
-			class=" bg-gradient-to-r from-blue-400 to-purple-400  box-decoration-clone bg-clip-text text-7xl font-bold text-transparent"
+			class=" bg-gradient-to-r from-blue-400 to-purple-400 box-decoration-clone bg-clip-text text-7xl font-bold text-transparent"
 		>
 			SvelteKit Enterprise Stack
 		</span>
